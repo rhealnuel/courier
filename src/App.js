a228283cbdb0cb3efb1cc6cfcfc34bb35eca8a91
Kawekwune Emmanuel <119399384+rhealnuel@users.noreply.github.com>
2024-06-27 14:41:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Navbar/>
       <Routes>
       
-      <Route path='/' element={<Homelang/>}  />
+      <Route path='/' element={<Home/>}  />
       <Route path='/track/:id' element={<Tracklang/>}  />
 
       </Routes>
